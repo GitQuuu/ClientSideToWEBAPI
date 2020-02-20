c2038b61866e5854dabebedca0f7bc73a42e91a5
Qu <Quan0027@elec.techcollege.dk>
2020-02-20 14:30:50 +0100

    Copying existing project to Repo

---
+++ ClientsideToWebAPI/ProjectsScripts/Settings.js
@@ -0,0 +1,35 @@
+﻿//const WEB_API_URL_StudentData = "http://localhost:62090/api/";
+
+
+
+const WEB_API_URL_StudentData = "http://webapistudentdata.buchwaldshave34.dk/api/";
+
+
+
+const WEB_API_URL_StudentData_Course = WEB_API_URL_StudentData + "Course";
+
+const WEB_API_URL_StudentData_Eductaion = WEB_API_URL_StudentData + "Education";
+
+const WEP_API_URL_USERINFO = WEB_API_URL_StudentData + "UserInfo";
+
+const WEB_API_URL_RETURN_CODES_AND_RETURN_STRINGS = WEB_API_URL_StudentData + "ReturnCodesAndStrings";
+
+
+
+const WEB_API_URL_StudentData_EducationLine = WEB_API_URL_StudentData + "EducationLine";
+
+const WEB_API_URL_StudentData_ContactForm = WEB_API_URL_StudentData + "ContactForm";
+
+
+
+const UserName = "h2_q";
+
+const Password = "q20";
+
+
+
+const jSonDecodingError = -100;
+
+const jSonDecodingOk = 0;
+
+const OperationOkHigherValueThanHere = 0;
